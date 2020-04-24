546dd173ce327346bf6c0e8cf8e67040d521bb56
Ruben Vitt <dev@rubeen.dev>
2020-04-24 17:54:01 +0200

    feat(alpha-blog): some more implementations

--- alpha-blog/app/controllers/concerns/articles_controller.rb
+++ alpha-blog/app/controllers/concerns/articles_controller.rb
@@ -2,4 +2,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    #a = params[:article]
+    #Article.new(title: a[:title], description: a[:description]).save
+    render plain: a
+  end
 end
