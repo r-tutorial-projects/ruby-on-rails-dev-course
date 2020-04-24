d4ad7820492ae0d7647875b988b0f692a3a6eb5b
Ruben Vitt <dev@rubeen.dev>
2020-04-24 15:18:13 +0200

    feat(alpha-blog): add migration-file adding new column to article files

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_124239) do
+ActiveRecord::Schema.define(version: 2020_04_24_125940) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
