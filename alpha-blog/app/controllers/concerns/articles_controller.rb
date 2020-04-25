9de02f0d13118a0ef04b0647cdcb4d4f2205929b
Ruben Vitt <dev@rubeen.dev>
2020-04-25 13:40:10 +0200

    feat(alpha-blog): implement editing articles

--- alpha-blog/app/controllers/concerns/articles_controller.rb
+++ alpha-blog/app/controllers/concerns/articles_controller.rb
@@ -8,12 +8,30 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
-    #a = params[:article]
-    #Article.new(title: a[:title], description: a[:description]).save
-    render plain: a
+    @article = Article.new(params.require(:article).permit :title, :description)
+    if @article.save
+      flash['notice'] = 'Article was created successfully'
+      redirect_to @article
+    else
+      render 'articles/new'
+    end
+  end
+
+  def update
+    @article = Article.find params[:id]
+    if @article.update(params.require(:article).permit :title, :description)
+      flash['notice'] = 'Article was updated successfully'
+      redirect_to articles_path
+    else
+      render 'articles/edit'
+    end
   end
 end
